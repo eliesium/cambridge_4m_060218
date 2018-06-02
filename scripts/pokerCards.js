d1fee14495736472dc6d2cd36422af8b7929a29e
jaycarlov5 <jay.valimento@gmail.com>
2018-06-02 23:16:09 +0800

    Updated PokerCards Section and poker.css

---
+++ scripts/pokerCards.js
@@ -0,0 +1,17 @@
+
+
+//Poker Cards
+var pokerCards = ['?','0', '1/2', '1', '2', '3','5','8','13','21','40','80','100','coffee break'];
+
+function CreatePokerCards()
+{
+	for (var i = 0; i < pokerCards.length; i++) {
+                       var btn = document.createElement("button");
+                       var t = document.createTextNode(pokerCards[i]);
+                       btn.appendChild(t);
+					   btn.className = 'pokerCardButtons'
+					   btn.value = pokerCards[i];
+                       document.getElementById('pokerCards').appendChild(btn);
+                    }
+	
+}
