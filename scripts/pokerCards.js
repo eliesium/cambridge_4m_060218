c18aa1d45ed0674f40dd5c04a872ad5e1e09eb24
eliesium <elieone00@gmail.com>
2018-06-05 17:49:56 +0800

    Added button functionality

--- scripts/pokerCards.js
+++ scripts/pokerCards.js
@@ -1,34 +1,9 @@
-/* 
-var app = angular.module('pokerApp', []);
-
-
-
-function pokerController($scope) {
-	$scope.count = 0;
-}
-
-app.directive("CreatePokerCards",function(){
-	return {
-		restrict: "E"
-		template:
-		
+var app = angular.module("PokerCardApp", []);
+app.controller("PokerCardCtrl", function($scope){
+	$scope.selectedCard = "None";
+  $scope.participantName = "John Doe";
+  $scope.pokerCards = ['?','0', '1/2', '1', '2', '3','5','8','13','21','40','80','100','coffee break'];
+	$scope.updateSelected = function(card){
+		$scope.selectedCard = card;
 	}
-}); */
-
-//Poker Cards
-var pokerCards = ['?','0', '1/2', '1', '2', '3','5','8','13','21','40','80','100','coffee break'];
-
-function CreatePokerCards()
-{
-	for (var i = 0; i < pokerCards.length; i++) {
-                       var btn = document.createElement("button");
-                       var t = document.createTextNode(pokerCards[i]);
-                       btn.appendChild(t);
-					   btn.className = 'pokerCardButtons'
-					   btn.value = pokerCards[i];
-                       document.getElementById('pokerCards').appendChild(btn);
-                    }
-	
-}
-
-
+});
